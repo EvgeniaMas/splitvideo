17dc9de99d23545eef97678b35496d23c6e61897
Евгения Масликова < >
2018-04-05 19:36:44 +0300

    fixes

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,7 @@
-// $(document).ready(function() {
-// 		$(document).on('.open', 'click', function(event){
-// 			$('.close_menu').addClass('close_visible');
-// 			$(this).addClass('oppenned');
-
-// 			event.stopPropagation();
-// 		})
-// 		$(document).on('body', 'click', function(event) {
-// 			$('.open').removeClass('oppenned');
-// 		})
-// 		$(document).on('.close_visible', 'click', function(event){
-// 			$('.open').removeClass('oppenned');
-// 			$('.close_menu').removeClass('close_visible');
-// 			event.stopPropagation();
-// 		});
-// 	});
-
-
-
 $(document).ready(function() {
 		$(document).delegate('.open', 'click', function(event){
-			// $('.close_menu').addClass('close_visible');
 			$(this).addClass('oppenned');
 			$('.close_menu').addClass('closed');
-
 			event.stopPropagation();
 		})
 		$(document).delegate('body', 'click', function(event) {
@@ -34,41 +13,3 @@ $(document).ready(function() {
 			event.stopPropagation();
 		});
 	});
-
-
-
-
-// $(document).ready(function() {
-// 		$(document).delegate('.open', 'click', function(event){
-// 			$('.close_menu').addClass('close_visible');
-// 			$(this).addClass('oppenned');
-
-// 			event.stopPropagation();
-// 		})
-// 		$(document).delegate('body', 'click', function(event) {
-// 			$('.open').removeClass('oppenned');
-// 		})
-// 		$(document).delegate('.close_menu', 'click', function(event){
-// 			$('.open').removeClass('oppenned');
-// 			$('.close_menu').removeClass('close_visible');
-// 			event.stopPropagation();
-// 		});
-// 	});
-
-
-
-
-
-// $(document).ready(function() {
-// 		$(document).delegate('.open', 'click', function(event){
-// 			$(this).addClass('oppenned');
-// 			event.stopPropagation();
-// 		})
-// 		$(document).delegate('body', 'click', function(event) {
-// 			$('.open').removeClass('oppenned');
-// 		})
-// 		$(document).delegate('.cls', 'click', function(event){
-// 			$('.open').removeClass('oppenned');
-// 			event.stopPropagation();
-// 		});
-// 	});
