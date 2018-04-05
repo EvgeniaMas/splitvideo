132335a327cd09e945b5de1e4ecc5d9f4b71c2bf
Евгения Масликова < >
2018-04-05 20:00:54 +0300

    fixes menu more

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,31 @@
+// $(document).ready(function() {
+// 		$(document).delegate('.open', 'click', function(event){
+// 			$(this).addClass('oppenned');
+// 			$('.close_menu').addClass('closed');
+// 			event.stopPropagation();
+// 		})
+// 		$(document).delegate('body', 'click', function(event) {
+// 			$('.open').removeClass('oppenned');
+// 		})
+// 		$(document).delegate('.close_menu', 'click', function(event){
+// 			$('.open').removeClass('oppenned');
+// 			$('.close_menu').removeClass('closed');
+// 			event.stopPropagation();
+// 		});
+// 	});
+
+
 $(document).ready(function() {
-		$(document).delegate('.open', 'click', function(event){
-			$(this).addClass('oppenned');
-			$('.close_menu').addClass('closed');
-			event.stopPropagation();
-		})
-		$(document).delegate('body', 'click', function(event) {
-			$('.open').removeClass('oppenned');
-		})
-		$(document).delegate('.close_menu', 'click', function(event){
-			$('.open').removeClass('oppenned');
-			$('.close_menu').removeClass('closed');
-			event.stopPropagation();
-		});
-	});
+$('.open').on('click', function (e) {
+  $(this).addClass('oppenned');
+  $('.close_menu').addClass('closed');
+  // e.stopPropagation();
+})
+
+$('.close_menu').on('click', function (e) {
+  $('.open').removeClass('oppenned');
+  $('.close_menu').removeClass('closed');
+  // e.stopPropagation();
+})
+
+});
